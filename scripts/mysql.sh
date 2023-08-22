1a816c3140a91f74a5072a94c6aeac5e4807bd32
CompetitiveLin <zee_lin@foxmail.com>
2023-08-22 21:56:16 +0800

    infra update

--- scripts/mysql.sh
+++
@@ -1,7 +0,0 @@
-docker run -d --name mysql-container \
-    -e MYSQL_ROOT_PASSWORD=tiktokDB \
-    -e MYSQL_USER=tiktokDB \
-    -e MYSQL_PASSWORD=tiktokDB \
-    -e MYSQL_DATABASE=db_tiktok \
-    -p 3309:3306 \
-    mysql:latest --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
