dd899d8f7928322458f0e349a63f6aa8a2184114
zyxbend <zyx_bend@163.com>
2024-01-09 14:25:09 +0800

    refactor：调整rpc框架选型为grpc

--- kitex/kitex.sh
+++
@@ -1,9 +0,0 @@
-kitex -module tiktok -I ./ -v -service usersrv user.proto
-
-kitex -module tiktok -I ./ -v -service commentsrv comment.proto
-
-kitex -module tiktok -I ./ -v -service relationsrv relation.proto
-
-kitex -module tiktok -I ./ -v -service favoritesrv favorite.proto
-
-kitex -module tiktok -I ./ -v -service videosrv video.proto
