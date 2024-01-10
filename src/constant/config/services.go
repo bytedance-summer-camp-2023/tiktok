6289edeaa8df8ec4b91dce9a0a172e34bb6a2acf
zyxbend <zyx_bend@163.com>
2024-01-10 16:26:19 +0800

    feat：添加服务端口号相关配置

--- src/constant/config/services.go
+++ src/constant/config/services.go
@@ -5,38 +5,3 @@ const WebServiceAddr = ":37000"
 
 const AuthRpcServerName = "TikTok-AuthService"
 const AuthRpcServerPort = ":37001"
-
-const UserRpcServerName = "TikTok-UserService"
-const UserRpcServerPort = ":37002"
-
-const CommentRpcServerName = "TikTok-CommentService"
-const CommentRpcServerPort = ":37003"
-
-const FeedRpcServerName = "TikTok-FeedService"
-const FeedRpcServerPort = ":37004"
-
-const PublishRpcServerName = "TikTok-PublisherService"
-const PublishRpcServerPort = ":37005"
-
-const FavoriteRpcServerName = "TikTok-FavoriteService"
-const FavoriteRpcServerPort = ":37006"
-
-const MessageRpcServerName = "TikTok-MessageService"
-const MessageRpcServerPort = ":37007"
-
-const RelationRpcServerName = "TikTok-RelationService"
-const RelationRpcServerPort = ":37008"
-
-const RecommendRpcServiceName = "TikTok-Recommend"
-const RecommendRpcServicePort = ":37009"
-
-const Metrics = ":37099"
-const VideoProcessorRpcServiceName = "TikTok-VideoProcessorService"
-
-const VideoPicker = "TikTok-VideoPicker"
-const Event = "TikTok-Recommend"
-const MsgConsumer = "TikTok-MgsConsumer"
-
-const BloomRedisChannel = "TikTok-Bloom"
-
-const MaxVideoSize = 200 * 1024 * 1024
