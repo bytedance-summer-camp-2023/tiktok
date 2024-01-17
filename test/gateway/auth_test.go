aeaaaddb37be431f2861467ac482cf4821c993af
zyxbend <zyx_bend@163.com>
2024-01-17 15:15:17 +0800

    test：添加k6

--- test/gateway/auth_test.go
+++ test/gateway/auth_test.go
@@ -10,6 +10,30 @@ import (
 	"tiktok/src/gateway/models"
 )
 
+func TestDisplayRegister(t *testing.T) {
+
+	url := "http://127.0.0.1:37000/douyin/user/register?username=epicmo&password=epicmo"
+	method := "POST"
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	assert.Empty(t, err)
+
+	res, err := client.Do(req)
+	assert.Empty(t, err)
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		assert.Empty(t, err)
+	}(res.Body)
+
+	body, err := io.ReadAll(res.Body)
+	assert.Empty(t, err)
+	user := &models.LoginRes{}
+	err = json.Unmarshal(body, &user)
+	assert.Empty(t, err)
+	assert.Equal(t, 0, user.StatusCode)
+}
+
 func TestRegister(t *testing.T) {
 
 	url := "http://127.0.0.1:37000/douyin/user/register?username=" + uuid.New().String() + "&password=zyxbend"
