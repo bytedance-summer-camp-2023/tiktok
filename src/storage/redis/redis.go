f2baf7671f80e2ef0a0c15f5ae60084601e5387f
zyxbend <zyx_bend@163.com>
2024-01-19 15:26:37 +0800

    feat：添加redis集群配置

--- src/storage/redis/redis.go
+++ src/storage/redis/redis.go
@@ -3,18 +3,25 @@ package redis
 import (
 	"github.com/redis/go-redis/extra/redisotel/v9"
 	"github.com/redis/go-redis/v9"
+	"strings"
 	"tiktok/src/constant/config"
 )
 
-var Client *redis.Client
+var Client redis.UniversalClient
 
 func init() {
-	Client = redis.NewClient(&redis.Options{
-		Addr:     config.EnvCfg.RedisAddr,
+	addrs := strings.Split(config.EnvCfg.RedisAddr, ";")
+	Client = redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs:    addrs,
 		Password: config.EnvCfg.RedisPassword,
 		DB:       config.EnvCfg.RedisDB,
 	})
+
 	if err := redisotel.InstrumentTracing(Client); err != nil {
+		panic(err)
+	}
+
+	if err := redisotel.InstrumentMetrics(Client); err != nil {
 		panic(err)
 	}
 }
